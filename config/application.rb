0f3d8f45ea5efc595849e2a5a0f0f0ba332348b0
Dusan Orlovic <duleorlovic@gmail.com>
2017-11-25 08:19:48 +0100

    Add rubocop
    rubocop --auto-correct # to correct some files
    rubocop --auto-gen-config # generate .rubocop_todo.yml
    echo 'inherit_from: .rubocop_todo.yml' > .rubocop.yml # create .rubocop.yml

--- config/application.rb
+++ config/application.rb
@@ -1,17 +1,17 @@
 require_relative 'boot'
 
-require "rails"
+require 'rails'
 # Pick the frameworks you want:
-require "active_model/railtie"
-require "active_job/railtie"
+require 'active_model/railtie'
+require 'active_job/railtie'
 # require "active_record/railtie"
 require 'neo4j/railtie'
-require "action_controller/railtie"
-require "action_mailer/railtie"
-require "action_view/railtie"
-require "action_cable/engine"
-require "sprockets/railtie"
-require "rails/test_unit/railtie"
+require 'action_controller/railtie'
+require 'action_mailer/railtie'
+require 'action_view/railtie'
+require 'action_cable/engine'
+require 'sprockets/railtie'
+require 'rails/test_unit/railtie'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -19,9 +19,8 @@ Bundler.require(*Rails.groups)
 
 module PremestiSe
   class Application < Rails::Application
-    
     config.generators do |g|
-      g.orm             :neo4j
+      g.orm :neo4j
     end
 
     # Configure where to connect to the Neo4j DB
@@ -34,7 +33,6 @@ module PremestiSe
     #  or
     # config.neo4j.session.type = :embedded
     # config.neo4j.session.path = Rails.root.join('neo4j-db').to_s
-
 
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1

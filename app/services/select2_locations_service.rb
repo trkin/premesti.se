0a5b1d9403e935d15d1120a42e16eab897aed103
Dusan Orlovic <duleorlovic@gmail.com>
2018-06-05 13:51:32 +0200

    Use params to prevent injection attack

--- app/services/select2_locations_service.rb
+++ app/services/select2_locations_service.rb
@@ -5,10 +5,11 @@ class Select2LocationsService
 
   def perform
     query = I18n.available_locales.map do |locale|
-      "location.`name_#{locale}` =~ '(?iu).*#{@term}.*'"
+      "location.`name_#{locale}` =~ {term}"
     end.join(' OR ')
     results = Location.query_as(:location)
                       .where(query)
+                      .params(term: "(?iu).*#{@term}.*")
                       .pluck(:location)
     results.map do |location|
       { id: location.id, text: location.name_with_address }

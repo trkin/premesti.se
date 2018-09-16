bda2076c68e2636d4e0fa2d336bd2d79a3ba0e61
Dusan Orlovic <duleorlovic@gmail.com>
2018-09-16 20:38:35 +0200

    temp

--- app/controllers/admin/chats_controller.rb
+++ app/controllers/admin/chats_controller.rb
@@ -6,5 +6,6 @@ class Admin::ChatsController < AdminController
     elsif params[:status] == 'archived'
       @chats = @chats.archived
     end
+    @chats = @chats.where(archived_reason: params[:archived_reason]) if params[:archived_reason].present?
   end
 end
